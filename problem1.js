b851b96bd4ff0b787347b64233e502d910bf1011
vharish98 <vishwakarmaharish209@gmail.com>
2022-11-30 10:37:18 +0530

    prime no

---
+++ problem1.js
@@ -0,0 +1,13 @@
+ let num=13;
+ let count=0;
+ for (let i=1; i<=num;i++){
+    if (num%1==0){
+  count++;   
+}
+}
+  if (count==2){
+  console.log ("is a prime no ");
+}
+else {
+  console.log ("is not a prime no")
+}

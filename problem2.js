eeacf1eab92793a020c138e8ffb8fd758ad30a9c
vharish98 <vishwakarmaharish209@gmail.com>
2022-11-30 10:51:20 +0530

    pailindrome detected

---
+++ problem2.js
@@ -0,0 +1,16 @@
+ let str = "naman"
+ let bag= "";
+for (let i=str.length-1;i>=0;i++){
+
+   bag=bag+str[i];
+
+}
+ console.log (bag);
+
+if (bag==str)
+{
+ console.log ("is a pailendrome);
+}
+else {
+console.log ( "is no a pailindrome);
+}
